6b745129970ef457628dc9fe3e06fdc289d3e762
Stevelee <hoang.le.tn91@gmail.com>
2025-12-12 05:16:13 +0700

    Add event PriceList index

--- app/core/PriceList/Application/UseCases/IndexPriceList.php
+++ app/core/PriceList/Application/UseCases/IndexPriceList.php
@@ -5,6 +5,7 @@ namespace Core\PriceList\Application\UseCases;
 use Core\PriceList\Application\DTOs\CreatePriceListRequest;
 use Core\PriceList\Application\DTOs\IndexPriceListRequest;
 use Core\PriceList\Domain\Services\PriceListService;
+use Illuminate\Support\Facades\Event;
 
 class IndexPriceList
 {
@@ -12,6 +13,11 @@ class IndexPriceList
 
     public function handle(IndexPriceListRequest $dto)
     {
+        Event::dispatch("erp.pricelist.delete", [
+            'user_id' => $dto->created_by,
+            'business_id' => $dto->business_id,
+            ...$dto->toArray()
+        ]);
         return $this->service->index($dto->toArray());
     }
 }
