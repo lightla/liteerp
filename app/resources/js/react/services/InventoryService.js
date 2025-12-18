13642ae25136691c5860010bad392483c289c1df
Stevelee <hoang.le.tn91@gmail.com>
2025-12-18 12:31:10 +0700

    Update Inventory service

--- app/resources/js/react/services/InventoryService.js
+++ app/resources/js/react/services/InventoryService.js
@@ -4,8 +4,8 @@ const InventoryService = {
     add: (data) => api.post("/business-access/inventory", data),
     show: (id) => api.get("/business-access/inventory/" + id),
     list: (data) => api.get("/business-access/inventory"
-         + `?page=${data.page}&keywords=${data.keywords}${data.purchase_id 
-        ? '&purchase_id=' + data.purchase_id : ''}${data?.isOrder ? '&isOrder=1' : ''}`),
+         + `?page=${data.page}&keywords=${data.keywords}${data?.customer_group_id 
+                ? '&customer_group_id=' + data.customer_group_id : ''}`),
     update: (data) => api.put("/business-access/inventory/" + data.id, data),
 };
 export default InventoryService;
