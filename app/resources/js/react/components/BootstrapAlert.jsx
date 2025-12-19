dd659da664d9b84b81e33ed2dc8ffc911530c4c7
Stevelee <hoang.le.tn91@gmail.com>
2025-12-19 20:10:14 +0700

    Update BootstrapAlert component

--- app/resources/js/react/components/BootstrapAlert.jsx
+++ app/resources/js/react/components/BootstrapAlert.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 export default function BootstrapAlert({
     message = '',
-    type = 'warning'
+    type = 'warning',
+    title = 'Noted'
 }) {
     return <div className={"alert alert-"+type+" alert-dismissible fade show"}
         role="alert">
-        <strong>Note!</strong> {message}
+        <strong>{title}!</strong> {message}
         <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 }
