19c93a2d4aa04a0834a36503589f86f3fb6b9cf8
Stevelee <hoang.le.tn91@gmail.com>
2025-12-21 02:05:08 +0700

    Remove load channel module, because it is not working in laravel 12

--- app/app/Console/Commands/MakeModule.php
+++ app/app/Console/Commands/MakeModule.php
@@ -231,9 +231,6 @@ class MakeModule extends Command
                 if (file_exists("\$routePath/web.php")) {
                     \$this->loadRoutesFrom("\$routePath/web.php");
                 }
-                    if (file_exists("\$routePath/channels.php")) {
-                    \$this->loadRoutesFrom("\$routePath/channels.php");
-                }
             }
             protected function loadModuleCommands(): void
             {
@@ -263,12 +260,6 @@ class MakeModule extends Command
                 }
             }
         }
-        PHP);
-
-        File::put("{$basePath}/Infrastructure/routes/channels.php", <<<PHP
-        <?php
-
-        use Illuminate\Support\Facades\Broadcast;
         PHP);
 
         /* ------------------------------
