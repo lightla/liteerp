56b0eb48f91c9375e4a6fc41336d3bf73860fc4f
Stevelee <hoang.le.tn91@gmail.com>
2025-12-22 20:20:21 +0700

    Add adapter index for purchase

---
+++ app/core/Purchase/Application/DTOs/IndexPurchaseRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Core\Purchase\Application\DTOs;
+
+class IndexPurchaseRequest
+{
+    
+    public function __construct(
+        public ?string $keywords = null,
+        public ?string $status = null,
+        public ?string $order_by = null,
+        public int $created_by,
+        public int $business_id
+    ) {}
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            keywords: $data['keywords'] ?? null,
+            status: $data['status'] ?? null,
+            order_by: $data['order_by'] ?? 'DESC',
+            created_by: $data['user_id'],
+            business_id: $data['business_id']
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'keywords'   => $this->keywords,
+            'status'   => $this->status,
+            'created_by'    => $this->created_by,
+            'business_id'    => $this->business_id,
+            'order_by'  => $this->order_by
+        ];
+    }
+}
