582875cb080bf810f87cb1899222f494732fad99
Stevelee <hoang.le.tn91@gmail.com>
2026-01-01 02:29:23 +0700

    Add type for common IndexFieldType

--- app/app/Supports/Forms/IndexFieldType.php
+++ app/app/Supports/Forms/IndexFieldType.php
@@ -2,7 +2,7 @@
 namespace App\Supports\Forms;
 final class IndexFieldType {
     public const TEXT = 'text';
-    public const HTML = 'html';
+    public const LINK = 'link';
     public const BADGE = 'badge';
     public const CUSTOMBADGE = 'custom_badge';
     public const IMAGE = 'image';
