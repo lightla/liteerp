6a054e843b1a6d42daa89e4e8a6554c07eade565
Stevelee <hoang.le.tn91@gmail.com>
2026-01-08 00:11:08 +0700

    Add loading for dashboard overview

--- app/resources/js/react/pages/dashboard.jsx
+++ app/resources/js/react/pages/dashboard.jsx
@@ -4,29 +4,38 @@ import DashboardSummary from '../components/Dashboard/DashboardSummary'
 import BusinessChart from '../components/Dashboard/BusinessChart'
 import OverviewService from '../services/OverviewService';
 import { usePopup } from '../components/popups/PopupContext'
+import LoadingBox from '../components/LoadingBox'
 export default function Dashboard() {
     const { openPopup } = usePopup();
     const [summaryData, setSummaryData] = useState([]);
     const [businessChart, setBusinessChart] = useState([]);
-
+    const [loading,setLoading] = useState(true);
     useEffect(() => {
+        setLoading(true)
         OverviewService.list()
             .then((resp) => {
                 setSummaryData(resp.message.top);
                 setBusinessChart(resp.message.chart)
+                setLoading(false)
             })
             .catch((error) => {
                 openPopup({
                     type: 'error',
                     message: error.response?.data?.message ?? 'Error'
                 })
+                setLoading(false)
             })
        
     }, []);
     return <DashboardLayout>
         <div className='dashboard-box'>
-            <DashboardSummary summaryData={summaryData} />
-            <BusinessChart data={businessChart} title='Business chart' />
+            {loading ? <div className='mt-5'>
+                <LoadingBox/>
+            </div> : <div>
+                <DashboardSummary summaryData={summaryData} />
+                <BusinessChart data={businessChart} title='Business chart' />
+            </div> }
+            
         </div>
     </DashboardLayout>
 }
