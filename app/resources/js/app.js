359883f19c8bff6fdb899af7b81879ce3f9a2e1a
Stevelee <hoang.le.tn91@gmail.com>
2026-01-09 03:05:10 +0700

    Implement autoload css,js extensions

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,3 +1,6 @@
 import './bootstrap';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './admin'
+import.meta.glob('../../extensions/**/Resources/css/*.css', { eager: true })
+import.meta.glob('../../extensions/**/Resources/js/*.js', { eager: true })
+
