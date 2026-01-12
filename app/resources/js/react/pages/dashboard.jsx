a952daeb3bfee2218fada20c6419689032e2566d
hoanglee <hoang.le.tn91@gmail.com>
2026-01-12 22:59:54 +0700

    Add empty data for Dashboard

--- app/resources/js/react/pages/dashboard.jsx
+++ app/resources/js/react/pages/dashboard.jsx
@@ -5,7 +5,10 @@ import BusinessChart from '../components/Dashboard/BusinessChart'
 import OverviewService from '../services/OverviewService';
 import { usePopup } from '../components/popups/PopupContext'
 import LoadingBox from '../components/LoadingBox'
+import BootstrapAlert from '../components/BootstrapAlert';
+import { useI18n } from '../../i18n/useI18n';
 export default function Dashboard() {
+    const {t} = useI18n();
     const { openPopup } = usePopup();
     const [summaryData, setSummaryData] = useState([]);
     const [businessChart, setBusinessChart] = useState([]);
@@ -32,8 +35,16 @@ export default function Dashboard() {
             {loading ? <div className='mt-5'>
                 <LoadingBox/>
             </div> : <div>
-                <DashboardSummary summaryData={summaryData} />
+                {summaryData.length === 0 && businessChart.length === 0 ? <div className='mt-5 pt-5'>
+                    <div className='theme-title text-center'>
+                        <i className="bi bi-database-fill-check h1"></i>
+                        <p>{t("overview_nodata")}</p>
+                    </div>
+                </div> : <div>
+                    <DashboardSummary summaryData={summaryData} />
                 <BusinessChart data={businessChart} title='Business chart' />
+                </div> }
+                
             </div> }
             
         </div>
