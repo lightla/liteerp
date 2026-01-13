2bc8139ff2ff9a394e9964a89b209e39c5473b7b
hoanglee <hoang.le.tn91@gmail.com>
2026-01-14 05:13:25 +0700

    Add command setup application

---
+++ app/app/Console/Commands/Setup.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Facades\Storage;
+
+class Setup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:setup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+        $steps = [
+            [
+                'command' => 'cp -r ./.env.example .env',
+                'desc'    => 'Copy environment configuration file',
+            ],
+            [
+                'command' => 'php artisan key:generate',
+                'desc'    => 'Generate application encryption key',
+            ],
+            [
+                'command' => 'php artisan migrate',
+                'desc'    => 'Run database migrations',
+            ],
+            [
+                'command' => 'php artisan storage:link',
+                'desc'    => 'Create symbolic link for storage directory',
+            ],
+            [
+                'command' => 'chmod -R 777 ./storage',
+                'desc'    => 'Set write permissions for storage directory',
+            ],
+            [
+                'command' => 'chmod -R 777 ./extensions',
+                'desc'    => 'Set write permissions for extensions directory',
+            ],
+            [
+                'command' => 'php artisan jwt:generate-keys',
+                'desc'    => 'Generate private and public keys for JSON Web Token',
+            ],
+            [
+                'command' => 'npm install',
+                'desc'    => 'Install frontend package',
+            ],
+            [
+                'command' => 'npm run build',
+                'desc'    => 'Build frontend assets',
+            ],
+            [
+                'command' => 'php artisan app:overview',
+                'desc'    => 'Build cache overview',
+            ],
+        ];
+
+        foreach ($steps as $key => $value) {
+            $result = Process::run($value['command']);
+            echo $result->output();
+            if ($result->successful()) {
+                echo $this->info(__($value['desc']));
+            } else {
+                echo $this->error(__($value['desc']));
+                echo $result->errorOutput();
+            }
+        }
+        $this->markErpAsInstalled();
+        $this->info("Please visit: " . env('APP_URL') . "/dashboard/login");
+    }
+    function markErpAsInstalled(): void
+    {
+        $path = 'erp.installed';
+
+        if (!Storage::disk('local')->exists($path)) {
+            Storage::disk('local')->put(
+                $path,
+                'LiteERP installed at ' . now()->toDateTimeString()
+            );
+        }
+    }
+}
