e22178cea2c0a4aa9db1f58ef003d520ca913690
stevelee <hoang.le.tn91@gmail.com>
2026-01-29 01:48:57 +0700

    Add alias resource

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +9,19 @@ export default defineConfig({
             refresh: true,
         })
     ],
+
+    resolve: {
+        alias: {
+            '@i18n': path.resolve(__dirname, 'resources/js/i18n'),
+            '@components': path.resolve(__dirname, 'resources/js/react/components'),
+            '@pages': path.resolve(__dirname, 'resources/js/react/pages'),
+            '@common': path.resolve(__dirname, 'resources/js/react/common'),
+            '@redux': path.resolve(__dirname, 'resources/js/react/redux'),
+            '@libraries': path.resolve(__dirname, 'resources/js/react/libraries'),
+            '@wrappers': path.resolve(__dirname, 'resources/js/react/wrappers'),
+        }
+    },
+
     build: {
         chunkSizeWarningLimit: 1600
     }
