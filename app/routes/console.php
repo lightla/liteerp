410191a94f56816257a63511394f552a2a33b7aa
stevelee <hoang.le.tn91@gmail.com>
2026-01-30 15:48:21 +0700

    Implement run job, fix bug env key

--- app/routes/console.php
+++ app/routes/console.php
@@ -3,8 +3,13 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-if(env('ENV') === 'production') {
+if(env('APP_ENV') === 'production') {
     Schedule::command('app:overview')->daily();
 } else {
     Schedule::command('app:overview')->everyFiveMinutes();
 }
+Schedule::command(
+    'queue:work --queue=high,default,low --sleep=3 --tries=3 --timeout=30 --max-jobs=1'
+)
+->everyFiveSeconds()
+->withoutOverlapping();
