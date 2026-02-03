17f053234f3498f2cd8828ed7cd858ba68dd16f5
stevelee <hoang.le.tn91@gmail.com>
2026-02-03 18:15:43 +0700

    Remove install service old

--- app/core/Extension/Application/UseCases/MakeExtension.php
+++ app/core/Extension/Application/UseCases/MakeExtension.php
@@ -4,22 +4,18 @@ namespace Core\Extension\Application\UseCases;
 
 use Core\Extension\Application\DTOs\MakeExtensionCommand;
 use Core\Extension\Domain\Services\ExtensionService;
-use Core\Extension\Infrastructure\Supports\ExtensionInstall;
-use Core\Extension\Infrastructure\Supports\ExtensionInstallExecutor;
 use Illuminate\Support\Facades\DB;
 
 class MakeExtension
 {
-    public function __construct(private ExtensionService $service, 
-        private ExtensionInstall $install, private ExtensionInstallExecutor $exec) {}
+    public function __construct(private ExtensionService $service) {}
 
     public function handle(array $data)
     {
         DB::beginTransaction();
         $dto = MakeExtensionCommand::fromArray($data);
         $entity = $this->service->make($dto->toArray());
-        DB::commit();
-        $this->exec->execute($this->install->installPlan($entity));   
+        DB::commit(); 
         return $entity;
     }
 }
