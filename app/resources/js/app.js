7022c0b0601b91bd1e759f65ad2b4ec7bb910043
stevelee <hoang.le.tn91@gmail.com>
2026-02-06 05:08:38 +0700

    Change rule autoload Resource extension, implement ServiceProvider written javascript

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,6 +1,22 @@
 import './bootstrap';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './admin'
-import.meta.glob('../../extensions/**/Resources/css/*.css', { eager: true })
-import.meta.glob('../../extensions/**/Resources/js/*.js', { eager: true })
-
+import serviceProvider from './core/ServiceProvider';
+/**
+ * Autoload css 
+ */
+import.meta.glob('../../extensions/**/Resources/css/autoload.css', { eager: true })
+/**
+ * Autoload javascript file of extension
+ */
+const modules = import.meta.glob('../../extensions/**/Resources/js/autoload.js', { eager: true });
+serviceProvider.expect(Object.keys(modules).length)
+/**
+ * Implement JS from extension and auto call 
+ */
+Object.values(modules).forEach(mod => {
+  const ExtensionClass = mod.default
+  if (typeof ExtensionClass === 'function') {
+    new ExtensionClass() 
+  }
+});
